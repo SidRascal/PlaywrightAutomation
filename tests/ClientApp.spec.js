ad79e659a787f811625e335bc89c6cf933f023ed
SidRascal <sid.official.qa@gmail.com>
2026-06-03 20:57:00 +0530

    This commit contains special operators and 1 full e2e test

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -3,8 +3,9 @@ const { test, expect } = require('@playwright/test');
 test.only('Browser Context First PlayWright Test', async ({ page }) => {
     const products = page.locator(".card-body")
     const productName = "iphone 13 pro"
+    const email = "test1.user1@gmail.com"
     await page.goto("https://rahulshettyacademy.com/client/")
-    await page.locator("#userEmail").fill("test1.user1@gmail.com")
+    await page.locator("#userEmail").fill(email)
     await page.locator("#userPassword").fill("Test@1234")
     await page.locator("[value='Login']").click()
     await page.waitForLoadState('networkidle')
@@ -24,23 +25,46 @@ test.only('Browser Context First PlayWright Test', async ({ page }) => {
     }
     await page.locator("[routerlink*='cart']").click()
     await page.locator("div li").first().waitFor()
-    const productIsPresent = await page.locator("h3:has-text(productName)").isVisible()
+    const productIsPresent = await page.locator("h3:has-text(' iphone 13 pro')").isVisible()
     expect(productIsPresent).toBeTruthy()
     await page.locator("text=Checkout").click()
-    await page.locator("[placeholder*='Country']").pressSequentially("ind")
-    const dropDown = page.locator("section.ta-results")
+    await page.locator("[placeholder*='Country']").pressSequentially("ind", {delay:150})
+    const dropDown = page.locator(".ta-results")
     await dropDown.waitFor()
-    optionCount = await dropDown.locator("button").count()
+    const optionCount = await dropDown.locator("button").count()
     for(let i=0; i<optionCount; i++)
     {
-        buttonText = dropDown.locator("button").nth(i).textContent()
-        if (buttonText.includes("India"))
+        const buttonText = await dropDown.locator("button").nth(i).textContent()
+        if (buttonText === " India")
             {
                 await dropDown.locator("button").nth(i).click()
-                breakl̥
+                break
             }
     }
+    expect(page.locator(".user__name [type=text]").first()).toHaveText(email)
+    await page.locator(".action__submit ").click()
+    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ")
+    const productId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent()
+    console.log(productId)
 
-}
+    await page.locator("button[routerlink*='myorders']").click()
+
+    await page.locator("tbody").waitFor()
+  
+
+    const rows = await page.locator("tbody tr")
+    for (let i=0; i< await rows.count; i++)
+    {
+        const rowOrderId = await rows.nth(i).locator("th").textContent()
+        if(productId.includes(rowOrderId))
+        {
+            await rows.nth(i).locator("button").first().click()
+            break
+        }
+    }
+    const detailsPageOrderId = await page.locator(".col-md-6 .col-text").textContent()
+    await expect(productId.includes(detailsPageOrderId)).toBeTruthy() 
+
+}   
 );
 
